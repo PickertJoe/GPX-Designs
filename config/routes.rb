65ec2253513ebe2c3033de8f422c3cdc3511950e
dilbert250 <pickertjoe@gmail.com>
2019-10-01 15:55:01 -0500

    Fixing views and controllers naming issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root to: 'welcome#index'
+  root to: 'welcomes#index'
   resources :faqs, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
