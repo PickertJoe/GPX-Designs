859052df277d4d049dd8800d13ad40291a59747f
dilbert250 <pickertjoe@gmail.com>
2019-10-03 14:08:50 -0500

    Removing unecessary admin route, styling changes to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users
   root to: 'welcomes#index'
   resources :faqs, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
