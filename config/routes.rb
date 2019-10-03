32bf83efa4a967138e6e2741566745cfa3d146c7
dilbert250 <pickertjoe@gmail.com>
2019-10-03 09:44:38 -0500

    Creating devise user model; migrating database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'welcomes#index'
   resources :faqs, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
