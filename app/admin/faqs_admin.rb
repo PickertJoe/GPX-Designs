9d91aa56379e3a0d30d13773a661a5ae0cef9c54
dilbert250 <pickertjoe@gmail.com>
2019-10-03 09:37:39 -0500

    Cleaning faq view page, customizing faq admin dashboard

--- app/admin/faqs_admin.rb
+++ app/admin/faqs_admin.rb
@@ -5,22 +5,18 @@ Trestle.resource(:faqs) do
 
   # Customize the table columns shown on the index view.
   #
-  # table do
-  #   column :name
-  #   column :created_at, align: :center
-  #   actions
-  # end
+  table do
+    column :question
+    column :answer
+    actions
+  end
 
   # Customize the form fields shown on the new/edit views.
   #
-  # form do |faq|
-  #   text_field :name
-  #
-  #   row do
-  #     col(xs: 6) { datetime_field :updated_at }
-  #     col(xs: 6) { datetime_field :created_at }
-  #   end
-  # end
+  form do |faq|
+    text_field :question
+    text_field :answer
+  end
 
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
