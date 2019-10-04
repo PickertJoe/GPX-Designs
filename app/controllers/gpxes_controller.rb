cd000936d7f6417f3bddc2456699d200cfa63826
dilbert250 <pickertjoe@gmail.com>
2019-10-04 14:18:47 -0500

    Creating basic creation form for GPX files

--- app/controllers/gpxes_controller.rb
+++ app/controllers/gpxes_controller.rb
@@ -1,15 +1,15 @@
 class GpxesController < ApplicationController
   def new
-    @gpx = GPX.new
+    @gpx = Gpx.new
   end
 
   def create
-    @gpx = GPX.new(gpx_params)
+    @gpx = Gpx.new(gpx_params)
 
-    if @gpx.save
-      redirect_to user_path(current_user)
+    if @gpx.save!
+      root_path
     else
-      redirect_to gpx_new
+      redirect_to new_gpx_path
 
     end
   end
