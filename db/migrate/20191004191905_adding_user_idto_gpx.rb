851abed578893f2a318a885fddc0ca9ae14cd02c
dilbert250 <pickertjoe@gmail.com>
2019-10-04 14:20:31 -0500

    Adding user_id column to gpx file; migrating

---
+++ db/migrate/20191004191905_adding_user_idto_gpx.rb
@@ -0,0 +1,5 @@
+class AddingUserIdtoGpx < ActiveRecord::Migration[6.0]
+  def change
+    add_column :gpxes, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_163027) do
+ActiveRecord::Schema.define(version: 2019_10_04_191905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_10_04_163027) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "activity"
+    t.integer "user_id"
   end
 
   create_table "lat_longs", force: :cascade do |t|
