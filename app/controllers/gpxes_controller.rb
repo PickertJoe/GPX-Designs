242b67c47a3f4906bcca4d5a0fa7d6aa94864279
dilbert250 <pickertjoe@gmail.com>
2019-10-04 11:41:24 -0500

    Beginning to flesh out gpx controller

--- app/controllers/gpxes_controller.rb
+++ app/controllers/gpxes_controller.rb
@@ -1,4 +1,20 @@
 class GpxesController < ApplicationController
-  def create
+  def new
+    @gpx = GPX.new
+  end
 
+  def create
+    @gpx = GPX.new(gpx_params)
+
+    if @gpx.save
+      redirect_to user_path(current_user)
+    else
+      redirect_to gpx_new
+
+    end
+  end
+
+  def gpx_params
+    params.require(:gpx).permit(:name,:activity, :file)
+  end
 end
