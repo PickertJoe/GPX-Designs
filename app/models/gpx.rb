3735dac9d98dbf9aecef21dbdb795bd4f71fa4fa
dilbert250 <pickertjoe@gmail.com>
2019-10-05 13:26:09 -0500

    Found bug; I added two associations on the same line

--- app/models/gpx.rb
+++ app/models/gpx.rb
@@ -1,7 +1,8 @@
 class Gpx < ApplicationRecord
   has_one_attached :file
   belongs_to :user
-  has_many :elevation, :lat_long
+  has_many :elevation
+  has_many :lat_long
 
   validates_presence_of :name
   validates_presence_of :activity
