57343f358d23310f939422e3de86e5c70e621ef6
dilbert250 <pickertjoe@gmail.com>
2019-10-18 12:38:56 -0500

    Integrating parsing code into elevation model

--- app/models/elevation.rb
+++ app/models/elevation.rb
@@ -12,8 +12,28 @@ class Elevation < ApplicationRecord
 
     def parse_gpx_file
 
-      raw_file = url_for(gpx.file)
+      raw_file = Nokogiri::XML(open(url_for(gpx.file)))
+      elev_parse = []
+      time_array = []
 
+      # Pulling elevation data into array
+      doc.css("ele").each do |link|
+        elev_parse << link.text.to_f
+      end
 
+      # Pulling time data into array; also removes unnecessary date and time zone info
+      doc.css("time").each do |link|
+        time_array << Time.parse(link.text, "DD.MM.YYYY hh:mm:ss").to_i*1000
+      end
+
+      # Casting arrays as Daru vectors
+      elev_vector = Daru::Vector.new(elev_parse)
+      time_vector = Daru::Vector.new(time_array)
+
+      # Combining vectors into Daru data frame
+      elevation = Daru::DataFrame.new({
+        Time: time_vector,
+        Elevation: elev_vector
+      })
 
 end
