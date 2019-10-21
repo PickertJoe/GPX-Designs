5599213fa9185c3ae96c7c95a8a912674bdd5ba6
dilbert250 <pickertjoe@gmail.com>
2019-10-21 10:59:55 -0500

    Adding basic create functionality to elevation profile; preparing to test

--- app/controllers/elevations_controller.rb
+++ app/controllers/elevations_controller.rb
@@ -3,4 +3,22 @@ class ElevationsController < ApplicationController
     @elevation = @gpx.elevation.build
   end
 
+  def create
+    @elevation = @gpx.elevation.build(elevation_params)
+
+    if @gpx.save
+      redirect_to user_gpxes_path
+    else
+      redirect_to new_user_gpx_path
+    end
+  end
+
+
+
+
+  private
+    def elevation_params
+      params.require(:gpx).permit(:title,:x_title, :y_title, :gpx_id, :size)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :gpxes
   end
+
+  resources :elevations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
