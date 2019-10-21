f5b662c556b18b1edaf022c697c4f1403a7058bb
dilbert250 <pickertjoe@gmail.com>
2019-10-21 14:55:23 -0500

    Expanding elevation controller functionality

--- app/controllers/elevations_controller.rb
+++ app/controllers/elevations_controller.rb
@@ -1,5 +1,7 @@
 class ElevationsController < ApplicationController
-  before_action :set_gpx, only: [:index, :new]
+  before_action :set_gpx, only: [:index, :new, :create]
+  before_action :set_elevation, only: [:show, :edit, :update, :destroy]
+
 
 
   def new
@@ -10,22 +12,40 @@ class ElevationsController < ApplicationController
     @elevation = @gpx.elevation.build(elevation_params)
 
     if @elevation.save
-      redirect_to user_gpxes_path
+      redirect_to user_gpxes_path(current_user)
     else
-      redirect_to new_user_gpx_path
+      redirect_to new_gpx_elevation_path
     end
   end
 
+  def edit
+  end
 
+  def update
+    @elevation.update(elevation_params)
+    if @elevation.save
+      redirect_to user_gpxes_path(current_user)
+    else
+      redirect_to edit_elevation_path
+    end
+  end
 
+  def destroy
+    @elevation.destroy
+    redirect_to user_gpxes_path(current_user), notice: "Your gpx file has been successfully deleted."
+  end
 
   private
     def elevation_params
-      params.require(:gpx).permit(:title,:x_title, :y_title, :gpx_id, :size)
+      params.require(:elevation).permit(:chart_title,:x_title, :y_title, :gpx_id, :size)
     end
 
     def set_gpx
       @gpx = Gpx.find(params[:gpx_id])
+    end
+
+    def set_elevation
+      @elevation = Elevation.find(params[:id])
     end
 
 end
