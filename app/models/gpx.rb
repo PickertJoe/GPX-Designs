1f76ad1633ea5e1f7fa464821c124883589c82ba
dilbert250 <pickertjoe@gmail.com>
2019-11-05 16:00:45 -0600

    Cleaning up loose code

--- app/models/gpx.rb
+++ app/models/gpx.rb
@@ -8,33 +8,4 @@ class Gpx < ApplicationRecord
   validates_presence_of :activity
 
   enum activity: [:Hike, :Run, :Bike, :Swim, :Ski, :Snowboard]
-
-
-  def parse
-
-    doc = Nokogiri::XML(open(ActiveStorage::Blob.service.send(:path_for, file.key)))
-    elev_parse = []
-    time_array = []
-
-    # Pulling elevation data into array
-    doc.css("ele").each do |link|
-      elev_parse << link.text.to_f
-    end
-
-    # Pulling time data into array; also removes unnecessary date and time zone info
-    doc.css("time").each do |link|
-      time_array << Time.parse(link.text, "DD.MM.YYYY hh:mm:ss").to_i*1000
-    end
-
-    # Casting arrays as Daru vectors
-    elev_vector = Daru::Vector.new(elev_parse)
-    time_vector = Daru::Vector.new(time_array)
-
-    # Combining vectors into Daru data frame for use in plotting
-    @elev_df = Daru::DataFrame.new({
-      Time: time_vector,
-      Elevation: elev_vector,
-    },
-      order: [:Time, :Elevation])
-  end
 end
